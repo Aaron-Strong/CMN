01e36fcc25ac7e2d3fc8a642bca1e7e268936abc
Mehdi <zidanimehdi89@hotmail.com>
2020-06-11 20:11:32 +0100

    very basic login

---
+++ src/main/java/servlet/Loginservlet.java
@@ -0,0 +1,56 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.*;
+
+@WebServlet("/login")
+public class Loginservlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+
+
+            Connection myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pfa_medico?" +
+                    "user=mehdi" + "&" +
+                    "password=mehdi" +
+                    "&serverTimezone=UTC");
+
+            PreparedStatement myPreparedStatement = myConnection.prepareStatement(
+                    "select * from patient where ? = username and ? = password"
+            );
+            myPreparedStatement.setString(1,req.getParameter("login"));
+            myPreparedStatement.setString(2,req.getParameter("password"));
+            ResultSet myResult = myPreparedStatement.executeQuery();
+            if (myResult.next()) {
+                final String User = myResult.getString("username");
+             final    String Role = "patient";
+                        System.out.println("logged as"+myResult.getString("email"));
+                HttpSession session=req.getSession();
+
+                session.setAttribute("User",User);
+                session.setAttribute("Role",Role);
+                System.out.println("negatory");
+            }
+
+        } catch (Exception e) {
+req.setAttribute("error",e);
+            req.getRequestDispatcher("/WEB-INF/deverror.jsp").forward(req,resp);
+
+        }
+
+//System.out.println(" request login  attribute : "+req.getParameter("login"));
+
+
+    }
+}
---
+++ web/WEB-INF/login.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zidan
+  Date: 11/06/2020
+  Time: 13:07
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Login Form</title>
+</head>
+<body>
+<form method="post" action="login">
+<label for="Login">Login :</label>
+<input type="text" name="login" id="Login"/>
+<label for="Password">Password :</label>
+<input type="password" name="password" id="Password"/>
+<input type="submit" value="login">
+</form>
+</body>
+</html>
